4b8901223cc50f38973ba7e79ddbe4581ceba313
Robert R. Meyer <Blue.Dog.Archolite@gmail.com>
2013-04-10 15:07:29 -0500

    update to allow for password to be sent to redis

--- lib/similus/config.rb
+++ lib/similus/config.rb
@@ -6,13 +6,17 @@ module Similus
   
   class Config
     attr_accessor :backend
-    attr_accessor :redis_server
+    attr_accessor :redis_host
+    attr_accessor :redis_port
+    attr_accessor :redis_username
+    attr_accessor :redis_password
     attr_accessor :redis_db
     attr_accessor :logfile
 
     def initialize #:nodoc:
       self.backend      = :redis
-      self.redis_server = "localhost:6379"
+      self.redis_host   = "localhost"
+      self.redis_port   = "6379"
       self.redis_db     = 9
       self.logfile      = STDOUT
     end
--- lib/similus/redis.rb
+++ lib/similus/redis.rb
@@ -1,14 +1,15 @@
 module Similus
   def self.redis
     @redis ||= begin
-      host, port = config.redis_server.split(':')
-      ::Redis.new(:host => host, :port => port, :db => config.redis_db)
+      ::Redis.new(:host => config.redis_host, :port => config.redis_port,
+                         :username => config.redis_username, :password => config.redis_password,
+                         :thread_safe => true)
     rescue Exception => e
       config.logger.error "Error connecting redis server: #{e.message}"
       nil
     end
   end
-  
+
   def self.clear_database!
     @cache = {}
     redis.flushdb
